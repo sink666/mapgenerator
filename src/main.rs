415518830e76dfad5db9ed1baaeade8fe16b7004
sink666 <er.basilisk@gmail.com>
2024-10-31 23:00:55 +0000

    yank out extra functions and checks dumb

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 use std::fs::File;
 use std::io::Write;
 use rand::distributions::{Distribution, Uniform};
@@ -17,6 +18,7 @@ enum Colors {
 struct State {
     width: usize,
     height: usize,
+    iter_c: usize,
     header: String,
     img_buf: Vec<u8>,
     f_buf: Vec<Colors>
@@ -27,69 +29,14 @@ struct Point (usize, usize);
 const BLACK: RGB = RGB { r:0, g:0, b:0 };
 const WHITE: RGB = RGB { r:255, g:255, b:255 };
 
-impl State {
-    fn get_ibuf_pt(&self, x: usize, y: usize) -> Option<usize> {
-        let offset = (x * 3) + (y * self.width * 3);
-        if offset < (3 * self.width * self.height) {
-            Some(offset)
-        } else {
-            None
-        }
-    }
-
-    fn get_fbuf_pt(&self, x: usize, y: usize) -> Option<usize> {
-        let offset = x + (y * self.width);
-        if offset < (self.width * self.height) {
-            Some(offset)
-        } else {
-            None
-        }
-    }
-}
-
-fn new_state(w: usize, h: usize) -> State {
+fn new_state(w: usize, h: usize, i: usize) -> State {
     State {
         width: w,
         height: h,
+        iter_c: i,
         header: format!("P6 {} {} 255\n", w, h),
         img_buf: vec!(0; 3 * w * h),
         f_buf: vec!(Colors::White; w * h),
-    }
-}
-
-fn field_buf_to_img(state: &mut State) -> bool {
-    for y in 0..state.height {
-        for x in 0..state.width {
-            if let Some(pixel) = state.get_ibuf_pt(x, y) {
-                let c = match state.f_buf[state.get_fbuf_pt(x, y).unwrap()] {
-                    Colors::Black => { BLACK },
-                    Colors::White => { WHITE }
-                };
-
-                state.img_buf[pixel] = c.r;
-                state.img_buf[pixel + 1] = c.g;
-                state.img_buf[pixel + 2] = c.b;
-            } else {
-                return false
-            }
-        }
-    }
-
-    return true
-}
-
-fn noize_field(fbuf: &mut Vec<Colors>) {
-    let mut rng = rand::thread_rng();
-    let uni_random = Uniform::from(1..100);
-
-    for p in fbuf {
-        let blackorwhite = uni_random.sample(&mut rng);
-
-        *p = if blackorwhite < 45 {
-            continue
-        } else {
-            Colors::Black
-        }
     }
 }
 
@@ -108,7 +55,7 @@ fn should_be_wall(ux: usize, uy: usize, state: &State) -> bool {
     ];
 
     for p in testp {
-        if state.f_buf[state.get_fbuf_pt(p.0, p.1).unwrap()] == Colors::Black {
+        if state.f_buf.get(p.0 + (p.1 * state.width)).unwrap() == &Colors::Black {
             acc = acc + 1;
         }
     }
@@ -120,37 +67,65 @@ fn should_be_wall(ux: usize, uy: usize, state: &State) -> bool {
     }
 }
 
-fn iterate_landscape(state: &mut State) {
-    let mut fbuf_new: Vec<Colors> = Vec::new();
+fn gen_landscape(st: &mut State) {
+    let mut rng = rand::thread_rng();
+    let uni_random = Uniform::from(1..100);
 
-    for y in 0..state.height {
-        for x in 0..state.width {
-            if should_be_wall(x, y, &state) {
-                fbuf_new.push(Colors::Black);
-            } else {
-                fbuf_new.push(Colors::White);
-            }
+    for p in &mut st.f_buf {
+        let blackorwhite = uni_random.sample(&mut rng);
+
+        *p = if blackorwhite < 45 {
+            continue
+        } else {
+            Colors::Black
         }
     }
 
-    state.f_buf = fbuf_new.clone();
-    fbuf_new.clear();
+    let mut fbuf_new: Vec<Colors> = Vec::new();
+
+    for _ in 0..st.iter_c {
+        for y in 0..st.height {
+            for x in 0..st.width {
+                if should_be_wall(x, y, &st) {
+                    fbuf_new.push(Colors::Black);
+                } else {
+                    fbuf_new.push(Colors::White);
+                }
+            }
+        }
+    
+        st.f_buf = fbuf_new.clone();
+        fbuf_new.clear();
+    }
+}
+
+fn output_file(st: &mut State) -> io::Result<()> {
+    let mut f = File::create("test.ppm")?;
+
+    for y in 0..st.height {
+        for x in 0..st.width {
+            let i_ofs = (x * 3) + (y * st.width * 3);
+            let color = match st.f_buf.get(x + (y * st.width)).unwrap() {
+                &Colors::Black => BLACK,
+                &Colors::White => WHITE
+            };
+            
+            st.img_buf[i_ofs] = color.r;
+            st.img_buf[i_ofs + 1] = color.g;
+            st.img_buf[i_ofs + 2] = color.b;
+        }
+    }
+
+    f.write(st.header.as_bytes())?;
+    f.write(&st.img_buf)?;
+
+    Ok(())
 }
 
 fn main() {
-    let mut f = File::create("test.ppm").expect("couldn't create file");
-    let mut gstate = new_state(100, 100);
+    let mut gstate = new_state(100, 100, 20);
 
-    noize_field(&mut gstate.f_buf);
+    gen_landscape(&mut gstate);
 
-    for _ in 0..20 {
-        iterate_landscape(&mut gstate);
-    }
-
-    if field_buf_to_img(&mut gstate) == false {
-        panic!("field to ppm failed!");
-    }
-
-    f.write(gstate.header.as_bytes()).expect("failed to write header");
-    f.write(&gstate.img_buf).expect("failed to write image data");
+    output_file(&mut gstate).unwrap();
 }
